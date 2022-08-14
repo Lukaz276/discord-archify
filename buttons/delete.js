b05a5b20a2f17859119e6114c53bad8819d1d3f5
zelythia <zelythia@gmail.com>
2022-08-14 16:30:06 +0200

    Updated the delete button to not send a message

--- buttons/delete.js
+++ buttons/delete.js
@@ -1,25 +1,22 @@
-const { ButtonInteraction, Message, TextChannel } = require("discord.js");
-const { id } = require('../config.json');
-
+const {ButtonInteraction, Message, TextChannel} = require('discord.js');
+const {id} = require('../config.json');
 
 module.exports = {
-    customId : 'delete',
+  customId: 'delete',
 
-    /**
-     * @param {ButtonInteraction} interaction 
-     */
-    async execute(interaction){
-        /** @type {TextChannel} */
-        // @ts-ignore
-        var channel = await interaction.client.channels.fetch(interaction.channelId);
-        var msg = await channel.messages.fetch(interaction.message.id);
+  /**
+   * @param {ButtonInteraction} interaction
+   */
+  async execute(interaction) {
+    /** @type {TextChannel} */
+    // @ts-ignore
+    var channel = await interaction.client.channels.fetch(interaction.channelId);
+    var msg = await channel.messages.fetch(interaction.message.id);
 
-        if(msg.deletable){
-            await msg.delete();
-            await interaction.reply({content: "Deleted saved message", ephemeral: true});
-        }
-        else{
-            await interaction.reply({content: "Cannot delete this message", ephemeral: true});
-        }
-    },
+    if (msg.deletable) {
+      await msg.delete();
+    } else {
+      await interaction.reply({content: 'Cannot delete this message', ephemeral: true});
+    }
+  },
 };
--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,19 @@
+const io = require('@pm2/io');
+const {Client} = require('discord.js');
 
 module.exports = {
-    name: "ready",
-    once: true,
-    execute(client){
-        console.log(`Logged in as ${client.user.tag}`);
-    },
+  name: 'ready',
+  once: true,
+  /**
+   * @param {Client} client
+   */
+  async execute(client) {
+    console.log(`Logged in as ${client.user.tag}`);
+    console.log((await client.guilds.fetch()).map((guild) => guild.name));
+
+    setInterval(async function () {
+      console.log('Guilds: ' + client.guilds.cache.size);
+      console.log((await client.guilds.fetch()).map((guild) => guild.name));
+    }, 3600000);
+  },
 };
